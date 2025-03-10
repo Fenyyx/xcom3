92dc7233640be04754bc49fafba60fd211638315
Fenyyx <fenixg12@gmail.com>
2025-03-10 18:01:25 +0000

    Frontend,backend y mongo conectado

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express"
+import authRoutes from "./routes/auth.routes.js"
+import dotenv from "dotenv"
+import connectMongoDB from "./db/connectMongoDB.js"
+
+
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 8000
+
+
+app.use("/api/auth", authRoutes)
+
+
+
+app.listen(PORT, () => {
+    console.log(`server en el puerto ${PORT}`)
+    connectMongoDB()
+})
